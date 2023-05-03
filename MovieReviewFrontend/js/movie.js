17f98a836eae6f4383b9718c595b0bc09f4ea099
Carter Antrobus <carter.antrobus@ontariotechu.net>
2023-05-03 18:52:47 -0400

    Adjustments/Interface Tweaks

--- MovieReviewFrontend/js/movie.js
+++ MovieReviewFrontend/js/movie.js
@@ -1,59 +1,119 @@
 // Avoid `console` errors in browsers that lack a console.
-const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9a86223857739bb40fddc4667939dbdd&page=1";
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
+const url = new URL(location.href);
+const movieId = url.searchParams.get("id")
+const movieTitle = url.searchParams.get("title")
+
+const APILINK = 'https://BackendMoveSite.carterantrobus.repl.co/api/v1/reviews/';
 
 
 const main = document.getElementById("section");
-const form = document.getElementById("form");
-const search = document.getElementById("query");
+const title = document.getElementById("title");
 
-returnMovies(APILINK)
-function returnMovies(url){
-  fetch(url).then(res => res.json())
+title.innerText = movieTitle;
+
+const div_new = document.createElement('div');
+div_new.innerHTML = `
+  <div class="row">
+    <div class="column">
+      <div class="card">
+          <strong>New Review</strong>
+          <p><a>Review</a>
+            <input type="text" id="new_review" value="">
+          </p>
+          <p><a>Username</a>
+            <input type="text" id="new_user" value="">
+          </p>
+          <p><a href="#" onclick="saveReview('new_review', 'new_user')"><i class="fa-solid fa-share"></i></a>
+          </p>
+      </div>
+    </div>
+  </div>
+`
+main.appendChild(div_new)
+
+returnReviews(APILINK);
+
+function returnReviews(url){
+  fetch(url + "movie/" + movieId).then(res => res.json())
     .then(function(data){
-      console.log(data.results);
-      data.results.forEach(element => {
+      console.log(data);
+      data.forEach(review => {
         const div_card = document.createElement('div');
-        div_card.setAttribute('class', 'card');
+        div_card.innerHTML = `
+          <div class="row">
+            <div class="column">
+              <div class="card" id="${review._id}">
+                <p><a>Review: </a>${review.review}</p>
+                <p><a>Username: </a>${review.user}</p>
+                <p><a onclick="editReview('${review._id}','${review.review}', '${review.user}')"><i class="fa-solid fa-pen-to-square"></i></a> <a onclick="deleteReview('${review._id}')"><i class="fa-solid fa-eraser"></i></a></p>
+              </div>
+            </div>
+          </div>
+        `
 
-        const div_row = document.createElement('div');
-        div_row.setAttribute('class', 'row');
-
-        const div_column = document.createElement('div');
-        div_column.setAttribute('class', 'column');
-
-        const image = document.createElement('img');
-        image.setAttribute('class', 'thumbnail');
-        image.setAttribute('id', 'image');
-
-        const title = document.createElement('h3');
-        title.setAttribute('id', 'title');
-
-        const center = document.createElement('center');
-
-        title.innerHTML = `${element.title}<br><a href="movie.html?id=${element.id}&title=${element.title}">reviews</a>`;
-        image.src = IMG_PATH + element.poster_path;
-
-        center.appendChild(image);
-        div_card.appendChild(center);
-        div_card.appendChild(title);
-        div_column.appendChild(div_card);
-        div_row.appendChild(div_column);
-
-        main.appendChild(div_row);
+        main.appendChild(div_card);
       });
     });
 }
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  main.innerHTML = '';
+function editReview(id, review, user) {
 
-  const searchItem = search.value;
+  const element = document.getElementById(id);
+  const reviewInputId = "review" + id
+  const userInputId = "user" + id
 
-  if (searchItem) {
-    returnMovies(SEARCHAPI + searchItem);
-    search.value = "";
+  element.innerHTML = `
+              <p><a>Review</a>
+                <input type="text" id="${reviewInputId}" value="${review}">
+              </p>
+              <p><a>Username</a>
+                <input type="text" id="${userInputId}" value="${user}">
+              </p>
+              <p><a onclick="saveReview('${reviewInputId}', '${userInputId}', '${id}',)"><i class="fa-solid fa-share"></i></a>
+              </p>
+
+  `
+}
+
+function saveReview(reviewInputId, userInputId, id="") {
+  const review = document.getElementById(reviewInputId).value;
+  const user = document.getElementById(userInputId).value;
+
+  if (id) {
+    fetch(APILINK + id, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({"user": user, "review": review})
+    }).then(res => res.json())
+      .then(res => {
+        console.log(res)
+        location.reload();
+      });
+  } else {
+    fetch(APILINK + "new", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({"user": user, "review": review, "movieId": movieId})
+    }).then(res => res.json())
+      .then(res => {
+        console.log(res)
+        location.reload();
+      });
   }
-});
+}
+
+function deleteReview(id) {
+  fetch(APILINK + id, {
+    method: 'DELETE'
+  }).then(res => res.json())
+    .then(res => {
+      console.log(res)
+      location.reload();
+    });
+}
